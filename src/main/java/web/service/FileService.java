6523226577396a579d079109724fa7e81cb5c218
Kimviruscool <sinsa9122@naver.com>
2024-07-25 15:56:02 +0900

    2024-07-25

--- src/main/java/web/service/FileService.java
+++ src/main/java/web/service/FileService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.UUID;
 
 @Service
 public class FileService {
@@ -11,8 +12,20 @@ public class FileService {
     //[1] 파일 업로드 : 매개변수로 파일의바이트가 저장된 MultipartFile 인터페이스
     //업로드 된 파일명 반환
     public String fileUpload(MultipartFile multipartFile){
+        //1. 첨부 파일의 실제 파일 이름 추출
+            //+ 클라이언트 들이 서로 다른 파일내용의 같은 파일명으로 업로드 했을때 식별이 불가능.
+            //해경방안 : 1. UUID(고유성 보장하는 ID 규약) 2. 조합식별 설계(주로 업로드날짜/시간 와 파일명 조합)
+        String uuid = UUID.randomUUID().toString(); // 난수의 UUID 생성 , 임의으이 UUID 규약에 따른 문자열 생성
+        System.out.println("uuid = " + uuid);
+
         //1. 파일 실제 이름 추출
         String fileName = multipartFile.getOriginalFilename();
+
+        //UUID + 파일이름 합치기 , UUID와 파일명을 구분하는 문자 조합 , 파일명의_(언더바)가 존재하면 안된다.
+        //추후에 _(언더바) 기준으로 분리하면 ( [0]UUID [1]파일명 ) 으로 분리가능
+        //"문자열".replaceAll("기존문자", "새로운문자") : 만약에 문자열내 기존문자가 존재하면 새로운문자로 지환해서 반환
+        fileName = uuid +"_"+fileName.replaceAll("_","-"); //파일명 내에 (_) 가 존재하면 (-)로 변환
+
         System.out.println("fileName = " + fileName);
         //2. 첨부파일을 저장/복사/이동할 저장할 경로 만들기
         String uploadPath = "C:\\Users\\tj-bu-703-021\\Desktop\\Spring\\src\\main\\resources\\static\\upload\\";
